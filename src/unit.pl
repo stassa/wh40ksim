9eb7f44bca772a298bb91319bf9b96871f571801
YeGoblynQueenne@splinter <e.patsantzis17@imperial.ac.uk>
2017-11-24 13:09:58 +0000

    Moved model-set logic to unit module.

--- src/unit.pl
+++ src/unit.pl
@@ -1,5 +1,8 @@
-:-module(unit, [models_unit/3]).
+:-module(unit, [model_sets/2
+	       ,model_set_attacks/4
+	       ,models_unit/3]).
 
+:-use_module(src(model)).
 :-use_module(src(datasheets)).
 
 
@@ -41,3 +44,61 @@ models_unit(Cs, Un, Un-Us):-
 		,once(wargear(Nm, Wg, _Num))
 		)
 	       ,Us).
+
+
+
+%!	model_sets(+Unit, -Model_sets) is det.
+%
+%	Partition a Unit into sets of Models according to their stats.
+%
+%	Model_sets is a list of lists of models with the same number of
+%	Attacks listed on their profile and with the same equipped
+%	weapons (therefore, number of attacks listed on their weapons
+%	profiles).
+%
+%	@tbd This will certainly break once I change the definition of
+%	models_unit/3 to allow for different wargear choices for models
+%	in a unit.
+%
+model_sets(Us, Ss):-
+	findall(Gi
+	       ,(group_by((A,Wg)
+			,M
+			,(member(M, Us)
+			 ,model_value(M, 'A', A)
+			 ,model_value(M, wargear, Wg)
+			 )
+			,Gi)
+		)
+	       ,Ss).
+
+
+
+%!	model_set_attacks(+Model_set,+Models,+Profile_attacks,+Weapon_attacks)
+%!	is det
+%
+%	Determine profile and weapon attack numbers for a Model_set.
+%
+model_set_attacks([M1|Ms], M, Pa, Wa):-
+	length([M1|Ms], M)
+	,model_value(M1, 'A', Pa)
+	,model_value(M1, wargear, Wg)
+	,weapon(Wg,_,_,Type,_,_,_,_)
+	,weapon_attacks(Type, Wa).
+
+
+%!	weapon_attacks(+Type, -Attacks) is det.
+%
+%	Determine the number of Attacks a Weapon can make.
+%
+weapon_attacks(T, A):-
+	T =.. [_Type,A_]
+	% Datasheets define random attack numbers
+	% as d-N, which is a bit off with the way
+	% die sizes are defined. Needs fixin'.
+	,(   A_ = d-N
+	 ->  roll(1, N, A)
+	 ;   A_ = A
+	 ).
+
+
