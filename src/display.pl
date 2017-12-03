2b0356a22ef57a04ae0df2fb3142c2c4f9e076f9
YeGoblynQueenne@splinter <e.patsantzis17@imperial.ac.uk>
2017-12-03 16:36:14 +0000

    Cleaned pretty_print by moving findall/3's in their own preds. * This is helpful to reduce clutter and also suggest uniform treatment   of various data- for instance, currently unit_profiles/2, the new   predicate that collects all of a unit's model profiles, also handles   their printcase-ing, which is something that may be better left to the   actual printing predicate (in the interest of separation of concerns,   that is).

--- src/display.pl
+++ src/display.pl
@@ -34,34 +34,15 @@ pretty_print(datasheet, Id):-
 	unit(Id, _Name, Faction)
 	,battlefield_role(Id, Role)
 	,power(Id, Power)
-	,findall([Name_,M,WS,BS,S,T,W,A,Ld,Sv]
-		,(unit_profiles(Id,Name,M,WS,BS,S,T,W,A,Ld,Sv)
-		 ,printcase(Name, Name_)
-		 )
-		,Profiles)
-	,findall([Max_wounds,Min_wounds,Mods]
-		,damaged_profiles(Id,Max_wounds,Min_wounds,Mods)
-		,Dmg_profiles)
+	,unit_profiles(Id, Profiles)
+	,damaged_profiles(Id, Dmg_profiles)
 	/*,unit_composition(Id, Model_type, Number)
 	,unit_composition_options(Id,Model,Number_in,Substitute,Number_out,Power_mod)*/
-	,findall(Wargear-Wg_Number
-		,wargear(Id, Wargear, Wg_Number)
-		,WGs)
+	,unit_wargear(Id, WGs)
 	/*,wargear_options(Id,Item,Wg_Number_out,Wg_Substitute,Wg_Number_in)
 	,abilities(Id, Abilities)
 	,weapons(Id, Weapons)*/
-
-	,findall(weapon(Wg_Id,Profile,Range,Type,Wp_S,AP,D,Wp_Abilities)
-		,(member(Wg_Id-_Wg_Nums, WGs)
-		 ,weapon(Wg_Id,Profile,Range,Type,Wp_S,AP,D,Wp_Abilities)
-		 )
-		,Weapons)
-
-	/*,findall(weapon(Id,Profile,Range,Type,Wp_S,AP,D,Wp_Abilities)
-		,weapon(Id,Profile,Range,Type,Wp_S,AP,D,Wp_Abilities)
-		,Weapons)*/
-	,group_by_weapon_Id(Weapons,Grouped)
-
+	,unit_weapon_groups(WGs, Grouped)
 	/*,faction_keyword(Id, Fc_Keywords)
 	,keyword(Id, Keywords)*/
 	,print_header(user_output,Id,Faction,Role,Power)
@@ -71,6 +52,62 @@ pretty_print(datasheet, Id):-
 	.
 
 
+%!	unit_profiles(+Id,-Profiles) is det.
+%
+%	Collect Profiles of all models in a unit.
+%
+%	Id should be the unit's identifier.
+%
+unit_profiles(Id, Profiles):-
+	findall([Name_,M,WS,BS,S,T,W,A,Ld,Sv]
+	       ,(unit_profiles(Id,Name,M,WS,BS,S,T,W,A,Ld,Sv)
+		% Maybe leave that to printing preds?
+		,printcase(Name, Name_)
+		)
+	       ,Profiles).
+
+
+%!	damaged_profiles(+Id, -Profiles) is det.
+%
+%	Collect damage-induced modifiers to a unit's profile stats.
+%
+%	Id should be the identifier of a unit (not model).
+%
+damaged_profiles(Id, Profiles):-
+	findall([Max_wounds,Min_wounds,Mods]
+	       ,damaged_profiles(Id,Max_wounds,Min_wounds,Mods)
+	       ,Profiles).
+
+
+%!	unit_wargear(+Id, -Wargear) is det.
+%
+%	Collect default wargear options for a unit.
+%
+%	Id should be the identifier of a unit (not a model).
+%
+unit_wargear(Id, WGs):-
+	findall(Wargear-Wg_Number
+		,wargear(Id, Wargear, Wg_Number)
+		,WGs).
+
+%!	unit_weapon_groups(+Wargear,-Grouped) is det.
+%
+%	Group together a unit's Wargear with the same name.
+%
+%	This is used to group together different profiles of the same
+%	weapon. Grouped is a list-of-lists where each list is either a
+%	single element, meaning a weapon with a single profile, or
+%	multiple elements, each one profile of the same weapon.
+%
+unit_weapon_groups(Wargear, Grouped):-
+	findall(weapon(Wg_Id,Profile,Range,Type,Wp_S,AP,D,Wp_Abilities)
+	       ,(member(Wg_Id-_Wg_Nums, Wargear)
+		,weapon(Wg_Id,Profile,Range,Type,Wp_S,AP,D,Wp_Abilities)
+		)
+	       ,Weapons)
+	,group_by_weapon_Id(Weapons,Grouped).
+
+
 %!	print_header(+Stream,+Name,+Faction,+Battlefield_role,+Power)
 %!	is det.
 %
@@ -78,8 +115,9 @@ pretty_print(datasheet, Id):-
 %
 print_header(S,N,F,R,P):-
 	configuration:format_string(print_header,_Ft)
-	%,Sp = 22 % Column length; totally eyballed.
-	,Sp = 25 % Column length; also; matches rest rows' columns.
+        % Column length; totally eyballed.
+	% Matches rest rows' columns.
+	,Sp = 25
 	% Convert text to printcase
 	,findall(T_
 		,(member(T, [N,F,P,R])
@@ -90,15 +128,9 @@ print_header(S,N,F,R,P):-
 	% Print unit name
 	,format(S, '~*+~w~n',[Sp,N_])
 	% Print separator line
-	%,atom_length(F_,M) % Length of the last string to be printed
-	%,Ovrln is Sp * 2 + M % Length of the line before that.
-	%,Ovrln_ is Ovrln + 4 % Offset to match next row's length.
-	% Will need to do that automatically, or outside these printing preds.
-	%,format(S, '~`=t~*+~n', [Ovrln])
 	,print_overline(S,=)
 	,format(S, '~w ~*+~w ~*+~w~n',['Role',Sp,'Power',Sp,'Faction'])
 	,format(S, '~w ~*+~w ~*+~w~n',[R_,Sp,P_,Sp,F_])
-	%,format(S, Ft, [N_,Sp,F_,Sp,P_,Sp,R_])
 	.
 
 
@@ -120,10 +152,6 @@ print_profiles(Str, Profiles):-
 	      ,'A',Sp2
 	      ,'Ld',Sp2
 	      ,'Sv']
-	% Separating line
-	% |Name col. padding| + (|col. padding|-1 * |cols.|) + |last col.|
-	%,Ovrln is Sp1 + (Sp2 * 8) + 2
-	%,format(Str, '~`-t~*+~n', [Ovrln])
 	,print_overline(Str,-)
 	,format(Str,'~w ~*+~w ~*+~w ~*+~w ~*+~w ~*+~w ~*+~w ~*+~w ~*+~w ~*+~w~n',PH)
 	,forall(member([Name,M,WS,BS,S,T,W,A,Ld,Sv],Profiles)
@@ -143,10 +171,7 @@ print_profiles(Str, Profiles):-
 		     ,Sv_]
 		,format(Str,'~w ~*+~w ~*+~w ~*+~w ~*+~w ~*+~w ~*+~w ~*+~w ~*+~w ~*+~w~n',P)
 		)
-	       )
-	% And another one
-	%,format(Str, '~`-t~*+~n', [Ovrln])
-	.
+	       ).
 
 
 %!	print_damaged_profiles(+Stream, +Profiles) is det.
@@ -215,6 +240,12 @@ print_weapons(Str,Weapons):-
 %
 print_weapon_profiles(Stream,[weapon(Id,base,Range,Type,S,AP,D,Abilities)],Sp1,Sp2,Sp3):-
 	!
+	% Magic number used to print abilities in a column
+	% starting a few characters away from where the last
+	% weapon profile column ends. Totally eyballed but is
+	% basically the length of all columns before Ability
+	% plus a small offset.
+	,Mn = 63
         ,printcase(Id, Id_)
 	,P = [Id_,Sp1
 	     ,Range,Sp2
@@ -226,10 +257,9 @@ print_weapon_profiles(Stream,[weapon(Id,base,Range,Type,S,AP,D,Abilities)],Sp1,S
 	     ,forall(member(Ability,Abilities)
 			    ,(
 			       (    configuration:weapon_ability_text(Ability,Frmt,Args)
-			       ->   atom_concat('~64+',Frmt,F1)
-			           ,atom_concat(F1,'~n',F2)
-			           ,format(Stream, F2, Args)
-			       ;   true
+			       ->   atomic_list_concat([~,Mn,+,Frmt,~,n],'',F)
+			           ,format(Stream, F, Args)
+			       ;    true
 			      )
 			     )
 		    )
@@ -246,6 +276,7 @@ print_weapon_profiles(Stream,Profiles,Sp1,Sp2,Sp3):-
 	Profiles = [W|_]
 	,W = weapon(Id,_,_,_,_,_,_,_)
 	,printcase(Id, Id_)
+	,Mn = 63
 	,format(Stream,'~w~n',[Id_])
 	,forall(member(weapon(_,Profile,Range,Type,S,AP,D,Abilities), Profiles)
 	       ,(printcase(Profile,Profile_)
@@ -257,10 +288,8 @@ print_weapon_profiles(Stream,Profiles,Sp1,Sp2,Sp3):-
 		     ,D,Sp2
 		     ,forall(member(Ability,Abilities)
 			     ,(configuration:weapon_ability_text(Ability,Frmt,Args)
-			      %,format(Stream, '~64+~w~n', [Ability])
-			      ,atom_concat('~64+',Frmt,F1)
-			      ,atom_concat(F1,'~n',F2)
-			      ,format(Stream, F2, [Args])
+			      ,atomic_list_concat([~,Mn,+,Frmt,~,n],'',F)
+			      ,format(Stream, F, [Args])
 			      )
 			    )
 		     ,(	 Abilities = []
